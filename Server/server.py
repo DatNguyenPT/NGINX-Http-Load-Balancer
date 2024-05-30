d5177fe267e7b6535487197992fb3f7574b0ab0e
Dat Nguyen <datnpt2004@gmail.com>
2024-05-30 22:55:39 +0700

    Added 2 gauges

--- Server/server.py
+++ Server/server.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import prometheus_client
 from prometheus_client.core import CollectorRegistry
-from prometheus_client import Summary, Counter, Histogram, Gauge
+from prometheus_client import Summary, Counter, Histogram, Gauge, Info
 import os
 import time
 
@@ -12,9 +12,12 @@ app = Flask(__name__)
 
 _INF = float("inf")
 
+# Define Prometheus metrics
 graphs = {}
 graphs['c'] = Counter('python_request_operations_total', 'The total number of processed requests')
 graphs['h'] = Histogram('python_request_duration_seconds', 'Histogram for the duration in seconds.', buckets=(1, 2, 5, 6, 10, _INF))
+graphs['g'] = Gauge('python_server_status', 'Current status of the server')
+graphs['connections'] = Gauge('python_server_connections', 'Number of connections on each server')
 
 # Set the server ID from environment variable
 server_id = os.getenv('SERVER_ID', 'Unknown')
@@ -26,20 +29,32 @@ engine = create_engine(db_url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Keep track of the number of connections
+current_connections = 0
+
 @app.route('/')
 def home():
+    global current_connections
+
     start = time.time()
     graphs['c'].inc()
     time.sleep(0.600)
     end = time.time()
     graphs['h'].observe(end - start)
+    
+    # Set the server status gauge to 1 to indicate the server is active
+    graphs['g'].set(1)
+
+    # Increment the number of connections
+    current_connections += 1
+    graphs['connections'].set(current_connections)
 
     return f'This is server {server_id}'
 
 @app.route("/metrics")
 def requests_count():
     res = []
-    for k,v in graphs.items():
+    for k, v in graphs.items():
         res.append(prometheus_client.generate_latest(v))
     return Response(res, mimetype="text/plain")
 
